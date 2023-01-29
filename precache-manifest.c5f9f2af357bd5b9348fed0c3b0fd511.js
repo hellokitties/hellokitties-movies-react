00921452e036593a7a57624c7effea024a9809de
Iulia Litvin <iulia.litwin@yandex.ru>
2023-01-29 23:29:36 +0300

    Updates

---
+++ precache-manifest.c5f9f2af357bd5b9348fed0c3b0fd511.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f8d2bbbe87e91ffbd89",
+    "url": "/hellokitties-movies-react/static/css/main.70fb3945.chunk.css"
+  },
+  {
+    "revision": "9f8d2bbbe87e91ffbd89",
+    "url": "/hellokitties-movies-react/static/js/main.9f8d2bbb.chunk.js"
+  },
+  {
+    "revision": "022280761d4275f8b4b5",
+    "url": "/hellokitties-movies-react/static/js/1.02228076.chunk.js"
+  },
+  {
+    "revision": "450018540eb613d1882b",
+    "url": "/hellokitties-movies-react/static/js/runtime~main.45001854.js"
+  },
+  {
+    "revision": "71727b1d4470be06da5242d711ff7b53",
+    "url": "/hellokitties-movies-react/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/hellokitties-movies-react/precache-manifest.c5f9f2af357bd5b9348fed0c3b0fd511.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/hellokitties-movies-react/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
